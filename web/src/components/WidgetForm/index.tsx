39e5ebfb76bafd83fac85e276498729b99c26390
plaque700 <marcus-braga@hotmail.com>
2022-05-03 21:34:48 -0300

    Fix: Refatoração/organização do WidgetForm

---
+++ web/src/components/WidgetForm/index.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+import { CloseButton } from "../CloseButton";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+
+import bugImage from '../../assets/img/bug.svg';
+import ideaImage from '../../assets/img/idea.svg';
+import thoughtImage from '../../assets/img/thought.svg';
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+
+export const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      source: bugImage,
+      alt: 'Imagem de um inseto'
+    }
+  },
+  IDEA: {
+    title: 'Idea',
+    image: {
+      source: ideaImage,
+      alt: 'Imagem de uma lâmpada'
+    }
+  },
+  OTHER: {
+    title: 'Outro',
+    image: {
+      source: thoughtImage,
+      alt: 'Imagem de um nuvem de pensamento'
+    }
+  }
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm () {
+  
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
+  function handleRestartFeedback(){
+    setFeedbackType(null);
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+
+      {!feedbackType ? (
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+      ) : (
+        <FeedbackContentStep 
+          feedbackType={feedbackType}
+          onFeedbackRestartRequest={handleRestartFeedback}
+        />
+      )}
+
+      <footer className="text-xs text-neutral-400">
+        Feito com ♥ por <a href="https://github.com/plaque700" className="underline underline-offset-2">Plague700</a>
+      </footer>
+
+    </div>
+  )
+
+}
