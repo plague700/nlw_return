9e283dcba9893d30dbcfc19821bf0ebff5e41785
plaque700 <marcus-braga@hotmail.com>
2022-05-07 13:24:38 -0300

    Feat (Back-end): Utilizaçção do cors para liberação de acesso

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import { routes } from './routes';
 
 const app = express();
+
+// Liberação/não de acesso do backend (Liberado para geral k)
+app.use(cors());
+
+// Exemplo de restrição de acesso do cors
+// app.use(cors({
+//   origin: 'http://localhost:3000'
+// }));
 
 // Permite a interpretação de JSON
 app.use(express.json());
