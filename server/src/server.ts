f925379927e033ce8422947b0b353117d0c8fc01
plaque700 <marcus-braga@hotmail.com>
2022-05-07 10:32:01 -0300

    Feat (Back-end): Criação de rota de feedback

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,27 @@
-import express from 'express'
+import express from 'express';
+import { prisma } from './prisma';
+
 
 const app = express();
 
-app.get('/users', (req,res) => {
-  return res.send('Hello World');
+// Permite a interpretação de JSON
+app.use(express.json());
+
+app.post('/feedbacks', async (req,res) => {
+
+  const {type, comment, screenshot} = req.body;
+
+  // Criação de registro no Prisma
+  const feedback = await prisma.feedback.create({
+    data: {
+      type: type,
+      comment: comment,
+      screenshot: screenshot,
+    }
+  });
+
+  return res.status(201).json({data:feedback});
+
 });
 
 app.listen(3333, () => {
